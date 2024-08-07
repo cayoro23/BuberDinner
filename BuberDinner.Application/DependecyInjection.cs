80d8536ca71d92a516489cfab3cf2c70b5dda7e8
Carlos Rodriguez <carmanuel1997@gmail.com>
2024-08-06 19:15:17 -0500

    Agregando Dependency y package

---
+++ BuberDinner.Application/DependecyInjection.cs
@@ -0,0 +1,14 @@
+﻿using BuberDinner.Application.Services.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BuberDinner.Application;
+
+public static class DependecyInjection
+{
+    public static IServiceCollection AddAplication(this IServiceCollection services)
+    {
+        services.AddScoped<IAuthenticationService, AuthenticationService>();
+
+        return services;
+    }
+}
---
+++ BuberDinner.Infrastructure/DependecyInjection.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace BuberDinner.Infrastructure;
+
+public static class DependecyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        return services;
+    }
+}
