2d03a41a8d2a775643d91f0b5ce07235b784c619
Carlos Rodriguez <carmanuel1997@gmail.com>
2024-08-08 01:23:28 -0500

    Implementacion de manejo de errores

---
+++ BuberDinner.Api/Controllers/ErrorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.Api.Controllers;
+
+public class ErrorController : ControllerBase
+{
+    [Route("Error")]
+    public IActionResult Error()
+    {
+        return Problem();
+    }
+}
--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,16 +1,22 @@
+using BuberDinner.Api.Errors;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services
         .AddAplication()
         .AddInfrastructure(builder.Configuration);
+
     builder.Services.AddControllers();
+
+    builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
 }
 
 var app = builder.Build();
 {
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
