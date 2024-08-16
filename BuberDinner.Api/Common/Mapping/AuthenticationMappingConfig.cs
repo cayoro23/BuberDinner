80fc361160336edbd0630e59d1a0f6179d1a231c
Carlos Rodriguez <carmanuel1997@gmail.com>
2024-08-15 21:28:39 -0500

    Creacion de inkeccion de dependencias de Mapster

---
+++ BuberDinner.Api/Common/Mapping/AuthenticationMappingConfig.cs
@@ -0,0 +1,15 @@
+﻿using BuberDinner.Application.Authentication.Common;
+using BuberDinner.Contracts;
+using Mapster;
+
+namespace BuberDinner.Api.Common.Mapping;
+
+public class AuthenticationMappingConfig : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config.NewConfig<AuthenticationResult, AuthenticationResponse>()
+            .Map(dest => dest.Token, src => src.Token)
+            .Map(dest => dest, src => src.User);
+    }
+}
